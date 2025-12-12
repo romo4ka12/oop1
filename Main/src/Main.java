a7c1e6efe1a6f3d7aabca7ff8d69969e0cd846a6
Roman <konoromada@mail.ru>
2025-12-12 10:51:52 +0500

    test 2

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("io = " + i);
         }
     }
 }
