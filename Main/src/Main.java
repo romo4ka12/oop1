b23bad1e5b7a25bc93ae8264c3fec89bdc6a18cb
Roman <konoromada@mail.ru>
2025-12-18 18:21:32 +0500

    g

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.printf("Hello World!");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i =" + i);
+            System.out.println("i = " + i);
         }
     }
 }
